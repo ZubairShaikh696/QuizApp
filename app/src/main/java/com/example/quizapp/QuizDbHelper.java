77eff179c6b6f6dd11caccf888ab74684da05ef7
ZubairShaikh696 <67466633+ZubairShaikh696@users.noreply.github.com>
2020-08-25 02:32:58 +0500

    Add files via upload

---
+++ app/src/main/java/com/example/quizapp/QuizDbHelper.java
@@ -0,0 +1,89 @@
+package com.example.quizapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class QuizDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "MyAwesomeQuiz.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
+
+    public QuizDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuizContract.QuestionsTable.TABLE_NAME + " ( " +
+                QuizContract.QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuizContract.QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_ANSWER_NR + " INTEGER" +
+                ")";
+
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionsTable();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + QuizContract.QuestionsTable.TABLE_NAME);
+        onCreate(db);
+    }
+
+    private void fillQuestionsTable() {
+        Questions q1 = new Questions("Pakistan Capital is", "Islamabad", "Karachi", "Lahore", 1);
+        addQuestion(q1);
+        Questions q2 = new Questions("Pakistan independent Date", "14-Sep-1979", "14-Aug-1947", "14-Aug-1973",2);
+        addQuestion(q2);
+        Questions q3 = new Questions("Pakistan National Language is", "Panjabi", "Urdu", "Sindhi", 2);
+        addQuestion(q3);
+        Questions q4 = new Questions("Pakistan Flag Color is", "White", "Green", "Both", 3);
+        addQuestion(q4);
+        Questions q5 = new Questions("Pakistan is a Islamic Country", "Yes", "No", "None", 1);
+        addQuestion(q5);
+    }
+
+    private void addQuestion(Questions question) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuizContract.QuestionsTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuizContract.QuestionsTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuizContract.QuestionsTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuizContract.QuestionsTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuizContract.QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        db.insert(QuizContract.QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+    public List<Questions> getAllQuestions() {
+        List<Questions> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + QuizContract.QuestionsTable.TABLE_NAME, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Questions question = new Questions();
+                question.setQuestion(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_ANSWER_NR)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return questionList;
+    }
+}
